8ddcb1da6e6e5d13bb0914da3b852653ced7250c
Sebstion <sebstachowski@gmail.com>
2023-10-30 07:26:16 -0400

    Dodanie obslugi kilku wartosci.

--- data/views.py
+++ data/views.py
@@ -3,23 +3,25 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 
 from .models import Data
-from .serializers import *
+from .serializers import DataSerializer
 
 @api_view(['GET', 'POST'])
 def data_list(request):
     if request.method == 'GET':
         data = Data.objects.all()
-
-        serializer = DataSerializer(data, context={'request': request}, many=True)
-
+        serializer = DataSerializer(data, many=True)
         return Response(serializer.data)
 
     elif request.method == 'POST':
-        serializer = DataSerializer(data=request.data)
+        data_list = request.data
+
+        if not isinstance(data_list, list):
+            data_list = [data_list]
+
+        serializer = DataSerializer(data=data_list, many=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
-            
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['PUT', 'DELETE'])
@@ -30,10 +32,14 @@ def data_detail(request, pk):
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'PUT':
-        serializer = DataSerializer(data, data=request.data,context={'request': request})
+        data_list = request.data
+        if not isinstance(data_list, list):
+            data_list = [data_list]
+
+        serializer = DataSerializer(data, data=data_list, partial=True, many=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
